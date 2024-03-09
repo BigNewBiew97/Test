b703653b4ed879b17976ad7e5b268c59935a8d7e
BigNewBiew97 <161944965+BigNewBiew97@users.noreply.github.com>
2024-03-09 15:44:59 +0700

    Add files via upload

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -46,40 +46,6 @@ local Tabs = {
     Visual = Window:AddTab({ Title = "Visual", Icon = "scan-face" }),wait(1),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" }),wait(1),
 }
-
-if game.CoreGui:FindFirstChild("SpeedHubXOpenClose") then
-    game.CoreGui:FindFirstChild("SpeedHubXOpenClose"):Destroy()
-end
-
-local UIStroke = Instance.new("UIStroke")
-local UICorner = Instance.new("UICorner")
-
-local ScreenGui = Instance.new("ScreenGui")
-local ImageButton = Instance.new("ImageButton")
-
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Name = "SpeedHubXOpenClose"
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-
-ImageButton.Parent = ScreenGui
-ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton.BorderColor3 = Color3.fromRGB(255, 0, 0)
-ImageButton.Draggable = true
-ImageButton.Position = UDim2.new(0.102097899, 0, 0.0742971897, 0)
-ImageButton.Size = UDim2.new(0, 59, 0, 49)
-ImageButton.Image = "rbxassetid://15511158491"
-
-UICorner.Name = "MainCorner"
-UICorner.CornerRadius = UDim.new(0, 9)
-UICorner.Parent = ImageButton
-
-ImageButton.MouseButton1Down:connect(function()
-game:GetService("VirtualInputManager"):SendKeyEvent(true,127,false,game)
-game:GetService("VirtualInputManager"):SendKeyEvent(false,127,false,game)
-end)
-
 local Options = SpeedHubX.Options
 do
 
@@ -1687,6 +1653,38 @@ end)
             UpdateClient()
         end
     end)
+if game.CoreGui:FindFirstChild("SpeedHubXOpenClose") then
+    game.CoreGui:FindFirstChild("SpeedHubXOpenClose"):Destroy()
+end
+
+local UIStroke = Instance.new("UIStroke")
+local UICorner = Instance.new("UICorner")
+
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "SpeedHubXOpenClose"
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderColor3 = Color3.fromRGB(255, 0, 0)
+ImageButton.Draggable = true
+ImageButton.Position = UDim2.new(0.102097899, 0, 0.0742971897, 0)
+ImageButton.Size = UDim2.new(0, 59, 0, 49)
+ImageButton.Image = "rbxassetid://15511158491"
+
+UICorner.Name = "MainCorner"
+UICorner.CornerRadius = UDim.new(0, 9)
+UICorner.Parent = ImageButton
+
+ImageButton.MouseButton1Down:connect(function()
+game:GetService("VirtualInputManager"):SendKeyEvent(true,127,false,game)
+game:GetService("VirtualInputManager"):SendKeyEvent(false,127,false,game)
+end)
     
     Tabs.Main:AddButton({
         Title = "Join Discord - Speed Hub X",
